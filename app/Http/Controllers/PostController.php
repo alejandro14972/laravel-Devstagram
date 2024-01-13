ea1ee73eed4a1639884bb8818105575b86e0059c
Alejandro <agoal0@gmail.com>
2024-01-13 23:30:55 +0100

    nueva home - ver post de usuarios seguidos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
   { //creamos la clase usewr coomo parametro
     //dd($user->email);  // dd(auth()->user()); /* helper que nos dice que usuario esta registrado */
 
-    $posts = Post::where('user_id', $user->id)->paginate(4); //consulta de los post del usuario
+    $posts = Post::where('user_id', $user->id)->latest()->paginate(4); //consulta de los post del usuario
     //dd($posts); //puedo uasr ->get() para sacar todo
 
     return view('dashboard', [
